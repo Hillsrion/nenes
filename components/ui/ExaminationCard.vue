d5a17f76db8c4eeeb65e17eac60add1f3d883647
Ismaël Sebbane <ismael.sebbane@gmail.com>
2025-10-12 19:59:36 +0200

    feat: add ExaminationCard component for displaying self-examination steps with descriptions

---
+++ components/ui/ExaminationCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-secondary-light flex flex-col justify-center p-8">
+    <p class="text-primary font-normal text-[2rem]/7.5 mb-5">
+      {{ stepNumber }}
+    </p>
+    <CardContent :content="description" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import CardContent from "~/components/ui/CardContent.vue";
+
+defineProps({
+  stepNumber: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+</script>
