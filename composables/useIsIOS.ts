6229f55ab1b5f83071a53cc01266bc3d8fabda5b
Ismaël Sebbane <ismael.sebbane@gmail.com>
2025-10-26 02:16:58 +0200

    chore: failsafe ios check

--- composables/useIsIOS.ts
+++ composables/useIsIOS.ts
@@ -2,6 +2,7 @@ import { computed } from "vue";
 
 export function useIsIOS() {
   const isIOS = computed(() => {
+    if (!navigator) return false;
     return (
       navigator.userAgent.includes("iPhone") ||
       navigator.userAgent.includes("iPad")
